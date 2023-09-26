b4391a3e0fede7ce3eff6fdf5dc4fca9c2061201
Frank Greco <fgreco@javasig.com>
2023-09-26 11:58:40 -0400

    Added flush() so Milvus collection committed to storage

--- src/main/java/MilvusTest.java
+++ src/main/java/MilvusTest.java
@@ -6,15 +6,14 @@
 
 import io.milvus.client.MilvusClient;
 import io.milvus.client.MilvusServiceClient;
-import io.milvus.grpc.DataType;
-import io.milvus.grpc.GetCollectionStatisticsResponse;
-import io.milvus.grpc.ListDatabasesResponse;
+import io.milvus.grpc.*;
 import io.milvus.param.ConnectParam;
 import io.milvus.param.LogLevel;
 import io.milvus.param.R;
 import io.milvus.param.RpcStatus;
 import io.milvus.param.collection.*;
 import io.milvus.param.dml.InsertParam;
+import io.milvus.response.DescCollResponseWrapper;
 import io.milvus.response.GetCollStatResponseWrapper;
 
 import java.io.IOException;
@@ -42,106 +41,24 @@ public class MilvusTest {
         );
         milvusClient.setLogLevel(LogLevel.Debug);
 
+        /*
+         TBD - create utility package of these Milvus methods -fdg
+         */
         show_databases(milvusClient);
-        //show_collection_stats(milvusClient, COLLECTION_NAME);
         drop_collection(milvusClient, COLLECTION_NAME);
         drop_database(milvusClient, DB_NAME);
         
         list_database_names(milvusClient);
         create_database(milvusClient, DB_NAME);
         list_database_names(milvusClient);
-
-
-       /*
-        Create Milvus collection schema... you need datatype for each field
-            A Milvus Collection is like a RDBMS table
-        */
-        FieldType fieldType1 = FieldType.newBuilder()        // schema for the id of the entry
-                .withName(FIELD1)
-                .withDataType(DataType.Int64)
-                .withPrimaryKey(true)
-                .withAutoID(false)
-                .build();
-        FieldType fieldType2 = FieldType.newBuilder()       // schema for num words in sentence (just an arbitrary field)
-                .withName(FIELD2)
-                .withDataType(DataType.Int64)
-                .build();
-        FieldType fieldType3 = FieldType.newBuilder()       // schema for the actual vector
-                .withName(FIELD3)
-                .withDataType(DataType.FloatVector)
-                .withDimension(2)
-                .build();
-
-        System.out.println("CREATING COLLECTION [" + COLLECTION_NAME + "]............");
-
-        CreateCollectionParam createCollectionReq = CreateCollectionParam.newBuilder()      // Create collection
-                .withCollectionName(COLLECTION_NAME)
-                .withDescription("Test frank search")
-                .withShardsNum(2)
-                .addFieldType(fieldType1)
-                .addFieldType(fieldType2)
-                .addFieldType(fieldType3)
-                .withEnableDynamicField(true)
-                .build();
-        response = milvusClient.createCollection(createCollectionReq);
-        if (response.getStatus() != R.Status.Success.getCode()) {
-            System.out.println("***FAILURE: " + response.getMessage());
-        } else
-            System.out.println("Collection created... SUCCESS ***");
+        create_collection_and_populate(milvusClient, COLLECTION_NAME);
+        flushCollection(milvusClient, COLLECTION_NAME);         // You need to flush the collection to storage!
         
-        /*
-            Now that you have the Collection, create the data
-         */
-
-        System.out.println("CREATING DATA FOR [" + COLLECTION_NAME + "]............");
-
-        Random ran = new Random();
-        List<Long> sentence_id_array = new ArrayList<>();               // to store sentence_ids
-        List<Long> word_count_array = new ArrayList<>();                // to store num of words in that sentence
-        List<List<Float>> sentence_vector_array = new ArrayList<>();    // to store vector for that sentence
-
-        for (long i = 0L; i < 100; i++) {                               // simulate 100 sentences
-            sentence_id_array.add(i);
-            word_count_array.add(ran.nextLong(25));               // just pick random word length
-            List<Float> vector = new ArrayList<>();
-            for (int k = 0; k < 2; k++) {                               // simulated vector (size 2)
-                vector.add(ran.nextFloat());
-            }
-            sentence_vector_array.add(vector);
-        }
-
-        /*
-            Now inserting the data for the Collection
-         */
-        List<InsertParam.Field> fields = new ArrayList<>();
-        fields.add(new InsertParam.Field(FIELD1, sentence_id_array));
-        fields.add(new InsertParam.Field(FIELD2, word_count_array));
-        fields.add(new InsertParam.Field(FIELD3, sentence_vector_array));
-
-        System.out.println("INSERTING DATA INTO [" + COLLECTION_NAME + "]............");
-
-        InsertParam insertParam = InsertParam.newBuilder()
-                .withCollectionName(COLLECTION_NAME)
-                // .withPartitionName("partition1")
-                .withFields(fields)
-                .build();
-        milvusClient.insert(insertParam);
-
-        System.out.println("TESTING IF Milvus HAS COLLECTION [" + COLLECTION_NAME + "]............");
-
-        R<Boolean> respHasCollection = milvusClient.hasCollection(
-                HasCollectionParam.newBuilder()
-                        .withCollectionName(COLLECTION_NAME)
-                        .build()
-        );
-        if (respHasCollection.getData() == Boolean.TRUE) {
-            System.out.println("Collection exists.");
-        } else
-            System.out.println("Collection DOES NOT exist.");
+        hasCollection(milvusClient, COLLECTION_NAME);
+        show_collection_stats(milvusClient, COLLECTION_NAME);     /* investigate why returning 0 rows... */
         drop_database(milvusClient, DB_NAME);
         /*
-         A better test would be to test if the collection exists and then retrieve that collections' values.
-         Another test needs to test the Milvus vector search.
+         Now need to test query/search - TBD
          */
 
         milvusClient.close();
@@ -183,17 +100,28 @@ public class MilvusTest {
      */
     static void show_collection_stats(MilvusClient mc, String coll) {
         System.out.println("GETTING COLLECTION STATS FOR " + coll + " IN [" + DB_NAME + "]..............");
+        R<DescribeCollectionResponse> respDescribeCollection = mc.describeCollection(
+                // Return the name and schema of the collection.
+                DescribeCollectionParam.newBuilder()
+                        .withCollectionName(coll)
+                        .build()
+        );
+        DescCollResponseWrapper wrapperDescribeCollection = new DescCollResponseWrapper(respDescribeCollection.getData());
+        System.out.println("["+coll+"]: " + wrapperDescribeCollection);
+
         GetCollectionStatisticsParam param = GetCollectionStatisticsParam.newBuilder()
                 .withCollectionName(coll)
                 .build();
         R<GetCollectionStatisticsResponse> cresponse = mc.getCollectionStatistics(param);
         if (cresponse.getStatus() != R.Status.Success.getCode()) {
             System.out.println("**ERROR in getting collection stats. " + cresponse.getMessage());
-        }  else
+        }  else {
             System.out.println("Success in getting collection stats!!");
 
-        GetCollStatResponseWrapper wrapper = new GetCollStatResponseWrapper(cresponse.getData());
-        System.out.println("Row count: " + wrapper.getRowCount());
+            GetCollStatResponseWrapper wrapper = new GetCollStatResponseWrapper(cresponse.getData());
+            System.out.println("Row count: " + wrapper.getRowCount());
+            System.out.println("["+coll+"]: " + wrapperDescribeCollection);
+        }
     }
 
     /************************************************************
@@ -212,6 +140,9 @@ public class MilvusTest {
                    System.out.println("DB Dropped... SUCCESS ***");
     }
 
+    /************************************************************
+          create database
+       */
     static void create_database(MilvusClient mc, String dbname) {
         R<RpcStatus> response = null;
         System.out.println("CREATING DATABASE [" + DB_NAME + "]..............");
@@ -225,6 +156,9 @@ public class MilvusTest {
             System.out.println("DB Created... SUCCESS ***");
     }
 
+    /************************************************************
+             list all the databases in the Milvus server
+     */
     static void list_database_names(MilvusClient mc) {
         System.out.println("LISTING DATABASE NAMES............");
         R<ListDatabasesResponse> ldbresponse = mc.listDatabases();
@@ -235,6 +169,122 @@ public class MilvusTest {
             for (int i = 0; i < ldbresponse.getData().getDbNamesCount(); i++) {
                 System.out.println(ldbresponse.getData().getDbNames(i));
             }
+        }
+    }
+
+    /************************************************************
+            Create the field schemas, create the collection and insert some dummy data
+     */
+    static void create_collection_and_populate(MilvusClient mc, String coll) {
+        R<RpcStatus> response = null;
+        /*
+         Create Milvus collection schema... you need datatype for each field
+             A Milvus Collection is like a RDBMS table
+         */
+         FieldType fieldType1 = FieldType.newBuilder()        // schema for the id of the entry
+                 .withName(FIELD1)
+                 .withDataType(DataType.Int64)
+                 .withPrimaryKey(true)
+                 .withAutoID(false)
+                 .build();
+         FieldType fieldType2 = FieldType.newBuilder()       // schema for num words in sentence (just an arbitrary field)
+                 .withName(FIELD2)
+                 .withDataType(DataType.Int64)
+                 .build();
+         FieldType fieldType3 = FieldType.newBuilder()       // schema for the actual vector
+                 .withName(FIELD3)
+                 .withDataType(DataType.FloatVector)
+                 .withDimension(2)
+                 .build();
+
+         System.out.println("CREATING COLLECTION [" + coll + "]............");
+
+         CreateCollectionParam createCollectionReq = CreateCollectionParam.newBuilder()      // Create collection
+                 .withCollectionName(coll)
+                 .withDescription("Test frank search")
+                 .withShardsNum(2)
+                 .addFieldType(fieldType1)
+                 .addFieldType(fieldType2)
+                 .addFieldType(fieldType3)
+                 .withEnableDynamicField(true)
+                 .build();
+         response = mc.createCollection(createCollectionReq);
+         if (response.getStatus() != R.Status.Success.getCode()) {
+             System.out.println("***FAILURE: " + response.getMessage());
+         } else
+             System.out.println("Collection created... SUCCESS ***");
+
+         /*
+             Now that you have the Collection, create the data
+          */
+
+         System.out.println("CREATING DATA FOR [" + coll + "]............");
+
+         Random ran = new Random();
+         List<Long> sentence_id_array = new ArrayList<>();               // to store sentence_ids
+         List<Long> word_count_array = new ArrayList<>();                // to store num of words in that sentence
+         List<List<Float>> sentence_vector_array = new ArrayList<>();    // to store vector for that sentence
+
+         for (long i = 0L; i < 100; i++) {                               // simulate 100 sentences
+             sentence_id_array.add(i);
+             word_count_array.add(ran.nextLong(25));               // just pick random word length
+             List<Float> vector = new ArrayList<>();
+             for (int k = 0; k < 2; k++) {                               // simulated vector (size 2)
+                 vector.add(ran.nextFloat());
+             }
+             sentence_vector_array.add(vector);
+         }
+
+         /*
+             Now inserting the data for the Collection
+          */
+         List<InsertParam.Field> fields = new ArrayList<>();
+         fields.add(new InsertParam.Field(FIELD1, sentence_id_array));
+         fields.add(new InsertParam.Field(FIELD2, word_count_array));
+         fields.add(new InsertParam.Field(FIELD3, sentence_vector_array));
+
+         System.out.println("INSERTING DATA INTO [" + coll + "]............");
+
+         InsertParam insertParam = InsertParam.newBuilder()
+                 .withCollectionName(coll)
+                 // .withPartitionName("partition1")
+                 .withFields(fields)
+                 .build();
+         mc.insert(insertParam);
+    }
+
+    /************************************************************
+      Flush - data is sealed and then flushed to storage
+        */
+    static void flushCollection(MilvusClient mc, String coll) {
+        FlushParam param = FlushParam.newBuilder()
+                .addCollectionName(coll)
+                .build();
+        R<FlushResponse> response = mc.flush(param);
+        if (response.getStatus() != R.Status.Success.getCode()) {
+            System.out.println(response.getMessage());
+        } else {
+            System.out.println("FLUSH successful!");
+        }
+    }
+
+    /************************************************************
+            If database has this collection.  btw, a "collection" is like an RDBMS table
+     */
+    static boolean hasCollection(MilvusClient mc, String coll) {
+        System.out.println("TESTING IF Milvus HAS COLLECTION [" + coll + "]............");
+
+        R<Boolean> respHasCollection = mc.hasCollection(
+                HasCollectionParam.newBuilder()
+                        .withCollectionName(COLLECTION_NAME)
+                        .build()
+        );
+        if (respHasCollection.getData() == Boolean.TRUE) {
+            System.out.println("Collection exists.");
+            return true;
+        } else {
+            System.out.println("Collection DOES NOT exist.");
+            return false;
         }
     }
 }
